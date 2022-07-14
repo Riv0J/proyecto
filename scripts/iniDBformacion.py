1f21129ebb0c20a0a76b49b8c02025ded2bd835d
acatalas <alemar2477@gmail.com>
2022-07-14 10:57:19 +0200

    Añadido datos de prueba (usuarios y formacion) y he añadido un array de participantes para guardar el nombre de usuario de los participantes que se apuntan

--- scripts/iniDBformacion.py
+++ scripts/iniDBformacion.py
@@ -1,5 +1,19 @@
 import shelve
 
 database = shelve.open('DBformacion.shlv', flag ='n')
-database['DevNet'] ={'Nombre':'DevNet','Participantes':'0', 'Mínimos':'12'}
+database['DevNet'] = {
+    'Nombre':'DevNet',
+    'Número_Participantes':'2', 
+    'Mínimos':'12',
+    'Participantes': [
+        'usuario1',
+        'usuario2'
+    ]
+    }
+database['Big_Data'] = {
+    'Nombre':'Big Data',
+    'Número_Participantes':'0', 
+    'Mínimos':'8',
+    'Participantes': []
+    }
 database.close()
--- scripts/iniDBusuarios.py
+++ scripts/iniDBusuarios.py
@@ -8,4 +8,7 @@ token = h.hexdigest()
 database = shelve.open('DBusuarios.shlv', flag ='n')
 database['1'] = {'user':'eoi','passwd':'eoi'}
 database['2'] = {'user':'guest','passwd':'guest'}
+database['3'] = {'user': 'usuario1', 'passwd': 'usuario1'}
+database['4'] = {'user': 'usuario2', 'passwd': 'usuario2'}
+database['5'] = {'user': 'usuario3', 'passwd': 'usuario3'}
 database.close()
